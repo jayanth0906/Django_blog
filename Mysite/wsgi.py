b826764cb9b7d43f16aa3f44758c1c707bb28224
jayanth0906 <42557399+jayanth0906@users.noreply.github.com>
2019-01-19 13:08:39 -0600

    initial commit

--- Mysite/wsgi.py
+++ Mysite/wsgi.py
@@ -8,12 +8,12 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise.django import DjangoWhiteNoise
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mysite.settings')
 
 application = get_wsgi_application()
 
-application = DjangoWhiteNoise(application)
+
 
